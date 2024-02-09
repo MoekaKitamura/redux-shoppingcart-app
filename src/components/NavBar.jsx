1c95bfb5f4454e6f056dc8ab6773cf440deca8d0
MoekaKitamura <moeka.k76@gmail.com>
2024-02-09 20:39:50 +0900

    hero icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { CartIcon } from "../HeroIcons";
 
 const NavBar = () => {
+  const { amount } = useSelector((store) => store.cart);
+
   return (
     <nav>
       <div className="nav-center">
         <h3>Redux Shopping</h3>
         <div className="nav-container">
-          ロゴ
+          <CartIcon />
           <div className="amount-container">
-            <p className="total-amount">0</p>
+            <p className="total-amount">{amount}</p>
           </div>
         </div>
       </div>
